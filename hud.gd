37ad6d07e80c479c9118df57dba5628fd9c2cbd6
Tomoyuki UBE <ube@coder.okinawa.jp>
2023-04-24 19:21:23 +0900

    first commit

---
+++ hud.gd
@@ -0,0 +1,48 @@
+extends CanvasLayer
+
+signal start_game
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+
+func show_game_over():
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	await $MessageTimer.timeout
+	#yield($MessageTimer, "timeout")
+
+	$Message.text = "Dodge the\nCreeps!"
+	$Message.show()
+	# Make a one-shot timer and wait for it to finish.
+	await get_tree().create_timer(1).timeout
+	#yield(get_tree().create_timer(1), "timeout")
+	
+	$StartButtonContainer/StartButton.show()
+	
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+	
+
+func _on_message_timer_timeout():
+	$Message.hide()
+	pass # Replace with function body.
+
+
+func _on_start_button_pressed():
+	$StartButtonContainer/StartButton.hide()
+	emit_signal("start_game")
+	pass # Replace with function body.
